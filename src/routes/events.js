3b676568bfdd30ab85af58e7f8083933f46abc96
Kelvin Viana <kelvin_piantino@hotmail.com>
2023-12-05 12:32:34 -0300

    feat[05/12/23]: Criação da conexão direta com sql server usando node, limpeza de pastas vazias

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,153 +1,147 @@
-'use strict';
+"use strict";
 
-const axios  = require('axios');
+//const axios = require("axios");
+const sql = require("mssql");
+const config = {
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  server: process.env.SQL_SERVER,
+  database: process.env.SQL_DATABASE,
+  options: {
+    encrypt: true,
+  },
+};
 
-module.exports.register = async server => {
-
+module.exports.register = async (server) => {
   var rotas = [];
-  var rota =  0;
-  var routes = [];
+  var rota = 0;
+  //var routes = [];
 
   const buscaRota = async () => {
-      try {
-        return await axios.get('http://localhost:8080/Datasnap/rest/TdmAdmin/ApiAdmin/')
-      } catch (err) {
-          console.log('erro:', err)
+    try {
+      await sql.connect(config);
+      const request = new sql.Request();
+      const result = await request.execute(
+        "[EGISADMIN].[dbo].[pr_consulta_api]",
+        {
+          ic_parametro: 0,
+          nm_identificacao_api: "",
+          cd_api: 0,
+          cd_modulo: null,
+          dt_inicial: null,
+          dt_final: null,
+          cd_procedimento: null,
+          cd_tabela: null,
+        }
+      );
+      return result;
+    } catch (error) {
+      console.error("Erro ao executar a procedure:", err);
+    } finally {
+      await sql.close();
+    }
+
+    //try {
+    //  return await axios.get(
+    //    "http://localhost:8080/Datasnap/rest/TdmAdmin/ApiAdmin/"
+    //  );
+    //} catch (err) {
+    //  console.log("erro:", err);
+    //}
+  };
+
+  const getRota = async () => {
+    const rot = await buscaRota();
+    rotas = rot.recordset;
+    var s = {};
+
+    for (rota in rotas) {
+      console.log(rotas[rota].nm_identificacao_api + "-" + rotas[rota].cd_api);
+
+      if (rotas[rota].cd_tipo_retorno == 0) {
+        s = {
+          method: rotas[rota].nm_metodo,
+          config: {
+            cors: {
+              //origin: ["egisnet.com.br"],
+              origin: ["*"],
+              //headers: ["Access-Control-Allow-Methods","Access-Control-Allow-Headers", "Access-Control-Allow-Origin","Accept", "Authorization", "Content-Type", "If-None-Match", "Accept-language"],
+              //additionalHeaders: ["Access-Control-Allow-Headers: Origin, Content-Type, x-ms-request-id , Authorization"],
+              //AccessControlAllowMethods: ["*"]
+            },
+          },
+          path:
+            "/" + rotas[rota].nm_identificacao_api + rotas[rota].nm_parametro,
+          handler: async (request, h) => {
+            try {
+              const db = request.server.plugins.sql.client;
+              const api = request.url.pathname;
+              const bod = JSON.stringify(request.payload);
+              const rot = 0;
+
+              //rotas[rota].cd_api;
+              //const met = rotas[rota].nm_metodo;
+
+              const met = request.method;
+
+              const res = await db.events.getApi(api, bod, rot, met);
+
+              return res.recordset;
+            } catch (err) {
+              console.log("erro", err);
+            }
+          },
+        };
+      } else {
+        s = {
+          method: rotas[rota].nm_metodo,
+          config: {
+            cors: {
+              //origin: ["http://www.egisnet.com.br"],
+              origin: ["*"],
+              // headers: ["Access-Control-Allow-Methods:[GET, POST, PATCH, PUT, DELETE, OPTIONS]","Access-Control-Allow-Headers", "Access-Control-Allow-Origin","Accept", "Authorization", "Content-Type", "If-None-Match", "Accept-language"],
+              // additionalHeaders: ["Access-Control-Allow-Headers: Origin, Content-Type, x-ms-request-id , Authorization"],
+              // AccessControlAllowMethods: ["GET, POST, PATCH, PUT, DELETE, OPTIONS"],
+              //credentials: true
+            },
+          },
+          path:
+            "/" + rotas[rota].nm_identificacao_api + rotas[rota].nm_parametro,
+          handler: async (request, h) => {
+            try {
+              const db = request.server.plugins.sql.client;
+              const api = request.url.pathname;
+              const bod = JSON.stringify(request.payload);
+              const rot = 0;
+              //rotas[rota].cd_api;
+              // const met = rotas[rota].nm_metodo;
+              const met = request.method;
+
+              const res = await db.events.getApiEspecial(api, bod, rot, met);
+
+              //return res.recordset;
+              return JSON.parse(
+                JSON.parse(JSON.stringify(res.recordset[0].data))
+              );
+            } catch (err) {
+              console.log("erro", err);
+            }
+          },
+        };
       }
-  }
-  
-  const getRota = async() => {
-    console.log('ANTES events')
-      const rot = await buscaRota()
-      
-      //console.log(rot.data);
 
-      rotas = rot.data;
-      var s = {};
+      //routes.push( s );
+      server.route(s);
+    }
 
-      for (rota in rotas) {
+    //Rotas
 
-         //console.log(rotas);
-
-        //console.log(rotas[rota].cd_tipo_retorno);
-
-        console.log(rotas[rota].nm_identificacao_api+'-'+rotas[rota].cd_api );
-        
-        //console.log(rotas[rota]);
-
-        if ( rotas[rota].cd_tipo_retorno == 0 ) {
-
-           s = { method: rotas[rota].nm_metodo,               
-            config: {
-              cors: {
-                  //origin: ["egisnet.com.br"],
-                  origin: ["*"]
-                  //headers: ["Access-Control-Allow-Methods","Access-Control-Allow-Headers", "Access-Control-Allow-Origin","Accept", "Authorization", "Content-Type", "If-None-Match", "Accept-language"],
-                  //additionalHeaders: ["Access-Control-Allow-Headers: Origin, Content-Type, x-ms-request-id , Authorization"],
-                  //AccessControlAllowMethods: ["*"] 
-              }
-             },                    
-              path: '/' + rotas[rota].nm_identificacao_api + rotas[rota].nm_parametro,
-              handler: async (request, h) => { 
-                try {
-                  const db  = request.server.plugins.sql.client;
-                  const api = request.url.pathname;
-                  const bod = JSON.stringify(request.payload);
-                  const rot = 0; 
-                  
-                  //rotas[rota].cd_api;
-                  //const met = rotas[rota].nm_metodo;
-                  //console.log(bod);
-                  //console.log(met);
-                  // console.log(request.settings.payload);
-                  //console.log(request.method);
-
-                const met = request.method;
-
-                //  console.log(request.payload);
-                 //console.log(bod);
-
-                  const res = await db.events.getApi( api, bod, rot, met);
-
-                  //console.log(request.params);
-
-                  //console.log(api);
-                  //console.log(request);
-                  //console.log(res.recordset);
-
-
-                  return res.recordset;
-                  
-                } catch( err ){
-                    console.log('erro',err);
-                }                  
-              }
-             }
-
-            } else {
-
-              s = { method: rotas[rota].nm_metodo, 
-                config: {
-                  cors: {
-                      //origin: ["http://www.egisnet.com.br"],
-                      origin: ["*"]
-                     // headers: ["Access-Control-Allow-Methods:[GET, POST, PATCH, PUT, DELETE, OPTIONS]","Access-Control-Allow-Headers", "Access-Control-Allow-Origin","Accept", "Authorization", "Content-Type", "If-None-Match", "Accept-language"],
-                     // additionalHeaders: ["Access-Control-Allow-Headers: Origin, Content-Type, x-ms-request-id , Authorization"],
-                     // AccessControlAllowMethods: ["GET, POST, PATCH, PUT, DELETE, OPTIONS"],
-                      //credentials: true
-                  }
-              },                
-                path: '/' + rotas[rota].nm_identificacao_api + rotas[rota].nm_parametro,
-                handler: async (request, h) => { 
-                  try {
-                    const db  = request.server.plugins.sql.client;
-                    const api = request.url.pathname;
-                    const bod = JSON.stringify(request.payload);
-                    const rot = 0;
-                     //rotas[rota].cd_api;
-                   // const met = rotas[rota].nm_metodo;
-                   const met = request.method;
-
-                    //console.log(met);
-                    ////console.log(request);
-
-                    //console.log(request);
-                    //console.log(request.payload);
-                    //console.log(bod);
-
-
-
-                    const res = await db.events.getApiEspecial( api, bod, rot, met);
-                    
-  
-                    //console.log(api);
-                    
-                    //return res.recordset;    
-                    return JSON.parse(JSON.parse(JSON.stringify(res.recordset[0].data)));
-                    
-                  } catch( err ){
-                      console.log('erro',err);
-                  }                  
-                }
-               }
-              }  
-            
-        //routes.push( s );
-        server.route( s ); 
-
-      };
-
-     //Rotas
-     //console.log(routes);
-          
-      
-      /*
+    /*
       routes = [      { method: 'GET',
                    path: '/'+rotas[3].nm_identificacao_api,
                    handler: async (request, h) => { return 'xuxa' }
                  }];
 
-      console.log(routes);
       /*
 
    
@@ -155,7 +149,6 @@ module.exports.register = async server => {
     };
 
 
-      //console.log( rot.data);
       /*
       routes = [      { method: 'GET',
                    path: '/User',
@@ -176,22 +169,18 @@ module.exports.register = async server => {
                  }
                 ];
 
-                console.log(routes);
 
                 for (var route in routes) {
                     
                     server.route( routes[route] );
             
                 };
-         */    
-                            
+         */
+  };
 
-  }
-  
   getRota();
-     
-  
-    /*
+
+  /*
     async request => {
         try {
           const db = request.server.plugins.sql.client;
@@ -211,5 +200,4 @@ module.exports.register = async server => {
         handler: 
     });
    */
-
 };
